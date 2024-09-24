591dbb306ba48d40fe13a6ee6d2646f3bf615039
Gleison Lemos <gleison04lemos@gmail.com>
2024-09-24 19:38:54 -0300

    feat: create a memory db

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-// //new standard imports, define in packege.json: type:module
-// import { createServer } from 'node:http';
-
-// const server = createServer((request, response) => {
-//   response.write('Hello World');
-
-//   return response.end() // finish req
-// }) 
-
-// server.listen(3333)
-
 import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
 
 const server = fastify();
 
-server.get('/', () => {
+// POST http://localhost:3333/videos
+// PUT http://localhost:3333/videos/ID
+
+// Route Parameter ex: '3333/videos/:id'
+
+server.post('/videos', () => {
   return 'Hello World'
+})
+
+server.get('/videos', () => {
+  return 'Hello Rocketseat'
+})
+
+server.put('/videos/:id', () => {
+  return 'Hello Node.js'
+})
+
+server.delete('/videos/:id', () => {
+  return 'Hello Node.js'
 })
 
 server.listen({
