071a7147cad0a7ee2946b6d9648ebe92a030b75e
Gleison Lemos <gleison04lemos@gmail.com>
2024-09-24 18:21:07 -0300

    feat: starting project with fastify

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-//new standard imports, define in packege.json: type:module
-import { createServer } from 'node:http';
+// //new standard imports, define in packege.json: type:module
+// import { createServer } from 'node:http';
 
-const server = createServer((request, response) => {
-  response.write('Hello World');
+// const server = createServer((request, response) => {
+//   response.write('Hello World');
 
-  return response.end() // finish req
-}) 
+//   return response.end() // finish req
+// }) 
 
-server.listen(3333)
+// server.listen(3333)
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.get('/', () => {
+  return 'Hello World'
+})
+
+server.listen({
+  port: 3333,
+})
