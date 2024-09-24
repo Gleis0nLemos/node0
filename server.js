8cb3a24eec69570abff472b2af0b246ee6abd0bb
Gleison Lemos <gleison04lemos@gmail.com>
2024-09-24 18:03:25 -0300

    feat: end method

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //new standard imports, define in packege.json: type:module
 import { createServer } from 'node:http';
 
-const server = createServer(() => {
+const server = createServer((request, response) => {
   console.log('oi')
+
+  return response.end() // finish req
 }) 
 
 server.listen(3333)
