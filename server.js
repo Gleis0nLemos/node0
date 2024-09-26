651fedef99e2b49e0419302b0d0134300b0922b3
Gleison Lemos <gleison04lemos@gmail.com>
2024-09-26 15:25:50 -0300

    feat: method put

--- server.js
+++ server.js
@@ -26,8 +26,17 @@ server.get('/videos', () => {
   return videos
 })
 
-server.put('/videos/:id', () => {
-  return 'Hello Node.js'
+server.put('/videos/:id', (request, reply) => {
+  const videoId = request.params.id
+  const { title, description, duration } = request.body
+
+  database.update(videoId, {
+    title,
+    description,
+    duration,
+  })
+
+  return reply.status(204).send()
 })
 
 server.delete('/videos/:id', () => {
